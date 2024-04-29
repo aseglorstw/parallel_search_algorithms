29a3556eab6f4e112fb1c75e83530d355e9b8e13
aseglorstw <robert.bakhtigariev@gmail.com>
2024-04-29 19:25:57 +0200

    update: done.

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -4,17 +4,6 @@
 #include <queue>
 #include <omp.h>
 
-state_ptr get_best_result_bfs(std::vector<state_ptr> results) {
-    state_ptr best_result = nullptr;
-    for(auto result : results) {
-        if(best_result == nullptr) { best_result = result;}
-        else if (result->get_identifier() < best_result->get_identifier()) {
-            best_result = result;
-        }
-    }
-    return best_result;
-}
-
 bool is_visited(std::unordered_set<size_t>& visited, size_t id) {
     bool found;
     #pragma omp critical(visited_section)
@@ -38,6 +27,31 @@ void add_to_results(std::vector<state_ptr>& results, state_ptr neighbour_node) {
     }
 }
 
+void atomic_vector_merge(std::vector<state_ptr>& secondary_vector, std::vector<state_ptr>& local_vector) {
+    #pragma omp critical
+    {
+        secondary_vector.insert(secondary_vector.end(), local_vector.begin(),
+                                local_vector.end());
+    }
+    local_vector.clear();
+}
+
+void swap_vectors( std::vector<state_ptr>& main_vector, std::vector<state_ptr>& secondary_vector) {
+    main_vector.swap(secondary_vector);
+    secondary_vector.clear();
+}
+
+state_ptr get_best_result_bfs(std::vector<state_ptr> results) {
+    state_ptr best_result = nullptr;
+    for(auto result : results) {
+        if(best_result == nullptr) { best_result = result;}
+        else if (result->get_identifier() < best_result->get_identifier()) {
+            best_result = result;
+        }
+    }
+    return best_result;
+}
+
 state_ptr bfs(state_ptr root) {
     std::unordered_set<size_t> visited;
     visited.insert(root->get_identifier());
@@ -48,30 +62,22 @@ state_ptr bfs(state_ptr root) {
     {
         size_t threads = omp_get_num_threads();
         size_t thread_id = omp_get_thread_num();
+        std::vector<state_ptr> local_vector;
         while (!main_vector.empty() || results.size() <= 0) {
             for (size_t i = thread_id; i < main_vector.size(); i += threads) {
-                std::vector<state_ptr> local_secondary_vector;
                 auto current_node = main_vector[i];
                 for (auto neighbour_node: current_node->next_states()) {
                     if (!is_visited(visited, neighbour_node->get_identifier())) {
                         add_to_visited(visited, neighbour_node->get_identifier());
-                        local_secondary_vector.push_back(neighbour_node);
+                        local_vector.push_back(neighbour_node);
                         if(neighbour_node->is_goal()) { add_to_results(results, neighbour_node);}
                     }
                 }
-                #pragma omp critical
-                {
-                    secondary_vector.insert(secondary_vector.end(), local_secondary_vector.begin(),
-                                            local_secondary_vector.end());
-                }
-                local_secondary_vector.clear();
             }
+            atomic_vector_merge(secondary_vector, local_vector);
             #pragma omp barrier
             #pragma omp single
-            {
-                main_vector.swap(secondary_vector);
-                secondary_vector.clear();
-            }
+            swap_vectors(main_vector, secondary_vector);
         }
     }
     return get_best_result_bfs(results);
